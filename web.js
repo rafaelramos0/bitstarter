ad7afeb41e48b74993648518c1417196065a996d
Ubuntu <ubuntu@ip-172-31-14-205.us-west-2.compute.internal>
2013-07-19 19:23:26 +0000

    Update Hello World

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    var buffer = new buffer(fs.readFileSync(index.html));
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
