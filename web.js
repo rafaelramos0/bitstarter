ce476812f5ac3c548762d81517e41b5720e7743b
Ubuntu <ubuntu@ip-172-31-14-205.us-west-2.compute.internal>
2013-07-19 20:02:16 +0000

    Update web.js again

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-app.use(express.logger());
+//app.use(express.logger());
+app.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
+    reponse.setHeader('Content-Type', 'text/html');
     var buffer = fs.readFileSync('./index.html','utf-8'));
     response.send( buffer );
 //    response.send(fs.readFileSync('index.html').toString('utf-81');
