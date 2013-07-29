c1820a7834796fa0f03fc3b9d793855294812b40
rafaelramos0 <rafael.ramos0@gmail.com>
2013-07-29 21:16:27 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
 //    response.send(fs.readFileSync('index.html').toString('utf-81');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
